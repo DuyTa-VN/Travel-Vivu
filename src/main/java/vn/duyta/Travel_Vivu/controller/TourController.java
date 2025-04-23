b3b095076d9da779f0306470674ff15aba127c02
DuyTa-VN <duydt202@gmail.com>
2025-04-23 21:08:32 +0700

    Update Image Tours

--- src/main/java/vn/duyta/Travel_Vivu/controller/TourController.java
+++ src/main/java/vn/duyta/Travel_Vivu/controller/TourController.java
@@ -1,11 +1,22 @@
 package vn.duyta.Travel_Vivu.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import vn.duyta.Travel_Vivu.model.TourImage;
+import vn.duyta.Travel_Vivu.service.TourImageService;
+import vn.duyta.Travel_Vivu.util.error.IdInvalidException;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/tours")
+@RequestMapping("/api/v1/images")
 public class TourController {
+    private final TourImageService tourImageService;
+
+    @PostMapping("/{tourId}/upload")
+    public ResponseEntity<TourImage> uploadImage(@PathVariable Long tourId, @RequestParam MultipartFile file) throws IdInvalidException {
+        TourImage tourImage = this.tourImageService.uploadImage(tourId, file);
+        return ResponseEntity.ok().body(tourImage);
+    }
 }
